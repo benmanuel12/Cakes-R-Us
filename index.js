d74d32dd29e9762c35ea507b4da5655a7f22c15d
Ben Manuel <contact.ben.manuel@googlemail.com>
2017-07-06 14:58:56 +0100

    Added trial user form and list

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var app = express();
 var fs = require("fs");
 var path = require('path');
-
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var jsonfile = require('jsonfile')
+
+var app = express();
 
 // Allows images to be displayed at http://localhost:8080/public/filename.extension
 app.use(express.static('public'));
@@ -13,41 +14,45 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Used in the submission form page
+
 app.get('/index.htm', function (req, res) {
   res.sendFile( __dirname + "/" + "index.htm");
 })
 
 app.get('/process_get', function(req, res) {
   //Prepare output in JSON format
-  response = {
+  console.log('HERE');
+  var user = {
     first_name:req.query.first_name,
     last_name:req.query.last_name,
     nick_name:req.query.nick_name
   };
-  console.log(response);
-  res.end(JSON.stringify(response));
-})
-
-// This responds with instructions on the homepage
-app.get('/', function(req, res) {
-  console.log("Got a GET request for the homepage");
-
-  // Move inline style onto CSS file
-  res.sendFile(path.join(__dirname + '/homepage.html'));
-  app.use(express.static('public'));
-
+  console.log(user);
+  var file = 'users.json'
+  jsonfile.writeFile(file, user, {flag: 'a'}, function (err) {
+    console.error(err)
+  })
+  res.end('done');
 })
 
 // This responds a GET request for the /list_user page.
 app.get('/list_user', function (req, res) {
   console.log("Got a GET request for the /list_user page.")
-  res.send('<h1> <strong> Users: </strong> HAL, Ben, Gavin</h1>')
+  var file = 'users.json'
+  var user_list = jsonfile.readFileSync(file);
+  res.send('<h1> <strong> Users: </strong></h1>' + JSON.stringify(user_list));
 })
 
 // This responds a GET request for abcd, abxcd, ab123cd and so on
 app.get('/ab*cd', function(req, res) {
-  console.log("Got a GET requesr for /ab*cd");
+  console.log("Got a GET request for /ab*cd");
   res.send('Page Pattern Match');
+})
+
+// This responds with instructions on the homepage
+app.get('/', function(req, res) {
+  console.log("Got a GET request for the homepage");
+  res.sendFile(path.join(__dirname + '/homepage.html'));
 })
 
   var server = app.listen(8083, function() {
